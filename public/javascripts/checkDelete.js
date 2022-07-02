ab6b1a4cf009ff5a616df61c23988344edc2156e
doug0849 <etaomonk@gmail.com>
2022-07-02 13:30:53 +0800

    feat: add facebook login strategy

---
+++ public/javascripts/checkDelete.js
@@ -0,0 +1,6 @@
+function CheckDelete() {
+  if (confirm("Are you sure to delete ") === true)
+    return true;
+  else
+    return false;
+}   
---
+++ routes/modules/todos.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const router = express.Router()
+
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
+const db = require('../../models')
+const Todo = db.Todo
+
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/new', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId}})
+    .then(todo => { 
+      todo = todo.toJSON()
+      return res.render('edit', { todo })})
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === "on"
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => todo.destroy({ where: { id }}))
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
+module.exports = router
